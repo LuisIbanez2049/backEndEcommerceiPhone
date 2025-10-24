b184b162a4153b51c638c6711030b06318219406
unknown <luiszyzz248@gmail.com>
2025-10-24 00:43:43 -0300

    Agregue el archivo Dockerfile para poder hacer el deploy del servidor en Render

--- src/test/java/com/example/iphonedropp/IphonedroppApplicationTests.java
+++ src/test/java/com/example/iphonedropp/IphonedroppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.iphonedropp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class IphonedroppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.iphonedropp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class IphonedroppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
